e376294d8a3ee325eee44fe45def85e5ac715f4f
Aritra Sarkar <aritra1911@yahoo.com>
2021-04-12 12:44:54 +0530

    Disable barpadding

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 0;        /* vertical padding of bar */
+static const int sidepad            = 0;        /* horizontal padding of bar */
 static const char *fonts[]          = {
 	"Roboto Mono:style=Regular:antialias=true:pixelsize=12",
 	"Material Design Icons Desktop:pixelsize=12:antialias=true:autohint=true",
