a6b0f3365104d31865e53a7c57d0542e7eff03cb
Aritra Sarkar <aritra1911@yahoo.com>
2022-03-09 12:07:59 +0530

    Add a shortcut to spawn Tsoding's boomer

--- config.h
+++ config.h
@@ -44,10 +44,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "XaoS",     "xaos",     NULL,       0,            1,           -1 },
+	/* class             instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",            NULL,       NULL,       0,            1,           -1 },
+	{ "firefox",         NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "firefox-nightly", NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "XaoS",            "xaos",     NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -85,66 +86,68 @@ static const char *rangercmd[] = { "st", "ranger", NULL };
 static const char *change_wall[] = { "change_wall", NULL };
 static const char *change_wall_sub[] = { "change_wall", "toggle", NULL };
 static const char *prtscrcmd[]  = { "screenshot",  NULL };
+static const char *zoomer[] = {"/home/ray/codes/notmine/boomer/boomer", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_minus,  spawn,          {.v = decr_vol } },
-	{ MODKEY,                       XK_equal,  spawn,          {.v = incr_vol } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = mpc_toggle } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_g,      spawn,          {.v = change_wall} },
-	{ MODKEY|ShiftMask,             XK_g,      spawn,          {.v = change_wall_sub} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.01} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.01} },
-	{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
-	{ 0,                            XK_Print,  spawn,          {.v = prtscrcmd } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_r,      spawn,          {.v = rangercmd } },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = toggle_mute } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                     key           function        argument */
+	{ MODKEY,                       XK_quoteleft, spawn,          {.v = zoomer } },
+	{ MODKEY,                       XK_minus,     spawn,          {.v = decr_vol } },
+	{ MODKEY,                       XK_equal,     spawn,          {.v = incr_vol } },
+	{ MODKEY,                       XK_p,         spawn,          {.v = mpc_toggle } },
+	{ MODKEY|ShiftMask,             XK_p,         spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,    spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,         togglebar,      {0} },
+	{ MODKEY,                       XK_g,         spawn,          {.v = change_wall} },
+	{ MODKEY|ShiftMask,             XK_g,         spawn,          {.v = change_wall_sub} },
+	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,         incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,         incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.01} },
+	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.01} },
+	{ MODKEY|Mod1Mask,              XK_h,         incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_l,         incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_h,         incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_l,         incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_h,         incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod1Mask|ControlMask,  XK_l,         incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_0,         togglegaps,     {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,         defaultgaps,    {0} },
+	{ MODKEY,                       XK_y,         incrihgaps,     {.i = +1 } },
+	{ MODKEY,                       XK_o,         incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_y,         incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_o,         incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_y,         incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_o,         incrohgaps,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_y,         incrovgaps,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,         incrovgaps,     {.i = -1 } },
+	{ 0,                            XK_Print,     spawn,          {.v = prtscrcmd } },
+	{ MODKEY,                       XK_Return,    zoom,           {0} },
+	{ MODKEY,                       XK_Tab,       view,           {0} },
+	{ MODKEY,                       XK_q,         killclient,     {0} },
+	{ MODKEY,                       XK_r,         spawn,          {.v = rangercmd } },
+	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,         setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,         setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_m,         spawn,          {.v = toggle_mute } },
+	{ MODKEY,                       XK_space,     setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
+	{ MODKEY,                       XK_0,         view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,         tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,     focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,    focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,     tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,    tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                         0)
+	TAGKEYS(                        XK_2,                         1)
+	TAGKEYS(                        XK_3,                         2)
+	TAGKEYS(                        XK_4,                         3)
+	TAGKEYS(                        XK_5,                         4)
+	TAGKEYS(                        XK_6,                         5)
+	TAGKEYS(                        XK_7,                         6)
+	TAGKEYS(                        XK_8,                         7)
+	TAGKEYS(                        XK_9,                         8)
+	{ MODKEY|ShiftMask,             XK_q,         quit,           {0} },
 };
 
 /* button definitions */
